1dedc9e78acaf883dfdb72baf20f398c1dd520bd
romanciobanu <romanciobanu@SE-MacBook-Air-1308.local>
2023-11-24 17:12:37 +0200

    add gmail auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   get 'email/parse_emails'
+  get 'email/connect_gmail_smpt'
+  get 'oauth2callback', to: "email#callback"
+  get 'email/connect_gmail'
+  get 'auth/:provider/callback', to: 'sessions#googleAuth'
+  get 'auth/failure', to: redirect('/')
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
